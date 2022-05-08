4282225c9389df6058477e348744fe1b6840ea84
danihp6 <danihp6@gmail.com>
2022-05-08 21:18:07 +0200

    Fix publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,10 +1,13 @@
-const { exec } = require('child_process');
-
-exec("npm run build")
-exec("cpy src/dictionaries dist/dictionaries --flat")
-exec("npm version patch")
-exec("cpy package.json dist/ --flat")
-exec("cpy tsconfig.json dist/ --flat")
-exec("cpy node_modules dist/")
-exec("cd dist")
-exec("npm publish")
+const { exec } = require("child-process-async");
+async function prepublish() {
+  await exec("npm run build");
+  await exec("cpy src/dictionaries dist/dictionaries --flat");
+  await exec("cpy tsconfig.json dist/ --flat");
+  await exec("cpy node_modules dist/");
+  await exec("npm version patch --git-tag-version=false");
+  await exec("cpy package.json dist/ --flat");
+  await exec("cpy README.md dist/ --flat");
+  await exec("cd dist");
+  await exec("npm publish");
+}
+prepublish();
