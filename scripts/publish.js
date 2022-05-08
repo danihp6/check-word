39a6aab3da0c751cc3ad4dd948f565e1102217aa
danihp6 <danihp6@gmail.com>
2022-05-08 20:41:02 +0200

    Fix publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,14 +1,9 @@
 const { exec } = require('child_process');
-const path = require('path');
-const fs = require('fs');
-const packagePath = path.join(__dirname, '../package.json');
 
 exec("npm run build")
 exec("cpy src/dictionaries dist/dictionaries --flat")
+exec("npm version patch")
 exec("cpy package.json dist/ --flat")
 exec("cpy tsconfig.json dist/ --flat")
 exec("cpy node_modules dist/")
 exec("cd dist")
-const file = fs.readFileSync(packagePath).toString();
-const package = JSON.parse(file);
-console.log(+package.version + 0,01)
